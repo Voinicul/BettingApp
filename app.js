1bc75f1fcdad26b87386924c2b609e072b39b05f
Voinicul <voinadan2005@yahoo.com>
2022-06-24 23:38:29 +0300

    enter

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ document.querySelector('.home-submit-button').onclick = function () {
     homeGames = document.querySelector('.home-games').value;
     calculation = (((Number(homeGoals) / Number(homeGames)) * 100) / 100).toPrecision(2);
 
-    document.querySelector(".home-results").value = calculation + '%';
+    //document.querySelector(".home-results").value = calculation + '%';
     document.querySelector(".host-total-average-goals").value = calculation ;
     
     return false;
@@ -21,7 +21,7 @@ document.querySelector('.away-submit-button').onclick = function () {
     awayGames = document.querySelector('.away-games').value;
     calculation = (((Number(awayGoals) / Number(awayGames)) * 100) / 100).toPrecision(2);
 
-    document.querySelector(".away-results").value = calculation + '%';
+    //document.querySelector(".away-results").value = calculation + '%';
     document.querySelector(".guest-total-average-goals").value = calculation ;
     return false;
 }
@@ -33,7 +33,7 @@ document.querySelector('.total-average-button').onclick = function () {
     homeAverage = document.querySelector('.total-home-average').value;
     awayAverage = document.querySelector('.total-away-average').value;
     totalCalculation = ((Number(homeAverage) + Number(awayAverage)) / 2).toPrecision(3);
-    document.querySelector(".total-results").value = totalCalculation ;
+   // document.querySelector(".total-results").value = totalCalculation ;
     document.querySelector(".here-total-average-goals").value = totalCalculation ;
     
     return false;
@@ -60,8 +60,7 @@ document.querySelector('.calculate-chances').onclick = function () {
     } else if (totalChancesCalculation === 41 || totalChancesCalculation <= 50.99) {
         document.querySelector(".recomended-bet").value = totalChancesCalculation + '% ' + " Recommended bet : Over 2.5 goals! ";
     } else {
-        document.querySelector(".recomended-bet").value = totalChancesCalculation + '% ' + " Recommended bet : Over 3 (asian handicap bet) goals or over 3.5 goals ";
-
+        document.querySelector(".recomended-bet").value = totalChancesCalculation + '% ' + " Recommended bet : Over 3 (asian handicap bet) goals or over 3.5 goals! ";
     }
     return false;
 
